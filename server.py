7bf43c5a62dd0849b3d44edb8bc2e6a5797f5620
Brandon R. Stoner <monokrome@limpidtech.com>
2013-08-29 22:24:05 -0700

    Added settings to server.py.

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
 from importd import d
+
+import settings
 import json
 import sys
+
+
+d(**settings.defaults)
+
 
 @d('/')
 def index(request):
   return d.HttpResponse('Hello, world.')
-
