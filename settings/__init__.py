ffb1614f5453dac26d357986b70bed585e98ece3
Brandon R. Stoner <monokrome@limpidtech.com>
2013-08-30 18:08:11 -0700

    Added default middleware classes.

--- settings/__init__.py
+++ settings/__init__.py
@@ -8,5 +8,14 @@ defaults = {
     "django.contrib.staticfiles",
     "django.contrib.admin",
     "django.contrib.admindocs",
-  ]
+  ],
+
+  "MIDDLEWARE_CLASSES": (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  )
 }
